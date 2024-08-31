988b087d522c67ab22fc2405e7969a1373ff199e
soulking13 <soulking13umar@gmail.com>
2024-08-31 14:59:32 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://highnetworld.com/2024/08/vercel-test/` + encodeURI(path as string)
+					`https://highnetworld.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
